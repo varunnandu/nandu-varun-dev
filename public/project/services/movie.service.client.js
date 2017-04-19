ecc30203d827bacd2c20332a58aa8afac887baaa
rpatel1990 <rpatel1990@ccs.neu.edu>
2017-04-19 11:17:52 -0400

    little bit country

--- public/project/services/movie.service.client.js
+++ public/project/services/movie.service.client.js
@@ -6,19 +6,19 @@
 
 
     function MovieService($http) {
-        var popularMovieUrl = "http://api.themoviedb.org/3/discover/movie?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&sort_by=popularity.desc&language=en&page=PAGE";
-        var genreListUrl = "http://api.themoviedb.org/3/genre/movie/list?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var creditsUrl = "http://api.themoviedb.org/3/movie/MOVIEID/credits?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var videoUrl = "http://api.themoviedb.org/3/movie/MOVIEID/videos?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var popularMovieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&sort_by=popularity.desc&language=en&page=PAGE";
+        var genreListUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var creditsUrl = "https://api.themoviedb.org/3/movie/MOVIEID/credits?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var videoUrl = "https://api.themoviedb.org/3/movie/MOVIEID/videos?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
         var youtubeEmbedUrl = "https://www.youtube.com/embed/KEY";
-        var similarUrl = "http://api.themoviedb.org/3/movie/MOVIEID/similar?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var reviewsUrl = "http://api.themoviedb.org/3/movie/MOVIEID/reviews?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var upcomingUrl = "http://api.themoviedb.org/3/movie/upcoming?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var nowplayingUrl = "http://api.themoviedb.org/3/movie/now_playing?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var topratedUrl = "http://api.themoviedb.org/3/movie/top_rated?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var searchUrl = "http://api.themoviedb.org/3/search/movie?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en&query=TITLE&page=PAGE";
-        var detailsUrl = "http://api.themoviedb.org/3/movie/ID?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
-        var imageUrl = "http://image.tmdb.org/t/p/original/";
+        var similarUrl = "https://api.themoviedb.org/3/movie/MOVIEID/similar?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var reviewsUrl = "https://api.themoviedb.org/3/movie/MOVIEID/reviews?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var upcomingUrl = "https://api.themoviedb.org/3/movie/upcoming?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var nowplayingUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var topratedUrl = "https://api.themoviedb.org/3/movie/top_rated?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var searchUrl = "https://api.themoviedb.org/3/search/movie?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en&query=TITLE&page=PAGE";
+        var detailsUrl = "https://api.themoviedb.org/3/movie/ID?api_key=a1eeabc6332dbb0d0d874a6a8d6a6a29&language=en";
+        var imageUrl = "https://image.tmdb.org/t/p/original/";
 
         var api = {
             findPopularMovies: findPopularMovies,
